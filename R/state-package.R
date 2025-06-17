b73bf7bdb3bb8651d7c1b3723f024357e6cf5911
Samuel Calderon <samuel.calderon@uarm.pe>
2025-06-17 17:16:08 -0500

    enable <S7_object>@name for R < 4.3.0

--- R/state-package.R
+++ R/state-package.R
@@ -4,5 +4,8 @@
 ## usethis namespace: start
 #' @import S7
 #' @import rlang
+#'
+## enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
 ## usethis namespace: end
 NULL
