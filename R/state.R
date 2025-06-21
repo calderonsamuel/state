137a56177a72824d348ce0ffde25b1529e9d0652
Samuel Calderon <samuel.calderon@uarm.pe>
2025-06-21 01:19:14 -0500

    add better constrains for potential types

--- R/state.R
+++ R/state.R
@@ -1,13 +1,32 @@
+bundled_S3_definitions <- new_union(
+  class_data.frame,
+  class_Date,
+  class_factor,
+  class_POSIXct,
+  class_POSIXlt,
+  class_POSIXt,
+  class_formula
+)
+
+#'
 #' @export
 state <- new_class(
   name = "state",
   properties = list(
     value = class_any,
-    type = class_any
+    type = S7_object | bundled_S3_definitions | class_vector,
+    allow_null = new_property(
+      class = class_logical,
+      default = FALSE,
+      validator = function(value) {
+        if (length(value) != 1L || is.na(value)) "Should be TRUE or FALSE"
+      }
+    )
   ),
   validator = function(self) {
     if (!inherits(self@value, self@type)) {
-      sprintf("`value` is not a <%s>", self@type$class)
+      class_name <- if (S7_inherits(self@type)) self@type@name else self@type$class
+      sprintf("`value` is not a <%s> object", class_name)
     }
   }
 )
